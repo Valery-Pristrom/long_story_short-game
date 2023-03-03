39d8c6426ebc1244da68b0953b4af5707b81d000
Your Name <you@example.com>
2023-03-03 13:21:09 +0300

    feat: add icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.scss';
 import { Button } from './components/Button/Button';
+import { IconQuit } from './components/Icon/IconQuit';
+import { IconRightArrow } from './components/Icon/IconRightArrow';
+import { IconRules } from './components/Icon/IconRules';
 
 function App() {
   return (
     <div className="App">
-      <Button className='btn__settings' text='hello'/>
+      <Button className='btn__settings' text='HELLO' icon={<IconRightArrow className='icon'/>}/>
+      <IconRightArrow className='icon'/>
+      <IconQuit className='icon'/>
+      <IconRules className='icon'/>
     </div>
   );
 }
